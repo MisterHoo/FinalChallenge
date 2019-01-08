742d26eb93816df5e52a8f7dde9020afd9f7ced0
Yosua Hoo <yosu4.hoo@gmail.com>
2019-01-08 13:08:47 +0700

    Add Files and some UIKit in ResultViewController

---
+++ Final Challenge/Controller/Main/Result/ResultActionButtonTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  ResultActionButtonTableViewCell.swift
+//  Final Challenge
+//
+//  Created by Yosua Hoo on 08/01/19.
+//  Copyright © 2019 Yosua Hoo. All rights reserved.
+//
+
+import UIKit
+
+class ResultActionButtonTableViewCell: UITableViewCell {
+
+    
+    
+    @IBAction func ButtonAteFood(_ sender: Any) {
+    
+    }
+    
+    @IBAction func ButtonDidNotAte(_ sender: Any) {
+    
+    }
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Final Challenge/Controller/Main/Result/ResultSimilarTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ResultSimilarTableViewCell.swift
+//  Final Challenge
+//
+//  Created by Yosua Hoo on 08/01/19.
+//  Copyright © 2019 Yosua Hoo. All rights reserved.
+//
+
+import UIKit
+
+class ResultSimilarTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
