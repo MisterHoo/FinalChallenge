6d64a2ae93f86d8e251678dacad3472210efe3eb
Kennyzi Yusuf <ykennyzi@gmail.com>
2019-01-09 13:24:40 +0700

    nambhain akses buat camera

--- Final Challenge/Controller/Main/ScanViewController.swift
+++ Final Challenge/Controller/Main/ScanViewController.swift
@@ -15,12 +15,17 @@ class ScanViewController: UIViewController {
         super.viewDidLoad()
         
         let captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .photo
         
         guard let captureDevice = AVCaptureDevice.default(for: .video) else {return}
         guard let input = try? AVCaptureDeviceInput(device: captureDevice) else {return}
         captureSession.addInput(input)
         
         captureSession.startRunning()
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(previewLayer)
+        previewLayer.frame = view.frame
         // Do any additional setup after loading the view.
     }
     
