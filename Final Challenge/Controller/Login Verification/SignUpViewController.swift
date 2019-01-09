b2dfc4cf71ae3e97a4f6bfc094a6989d1b6ec259
Brandon Cornelius <brandoncorneliuss@gmail.com>
2019-01-09 11:30:56 +0700

    dummy sign up

--- Final Challenge/Controller/Login Verification/SignUpViewController.swift
+++ Final Challenge/Controller/Login Verification/SignUpViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var rePasswordTextField: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
