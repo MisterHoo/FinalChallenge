8216445aaa6c082cd29315d5b93e6079b4d85d3b
Yosua Hoo <yosu4.hoo@gmail.com>
2019-01-29 17:27:57 +0700

    Find OUT Animation and etc

---
+++ Final Challenge/Controller/Main/FindOut/FoodMightLikeFOCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  FoodMightLikeFOCollectionViewCell.swift
+//  Final Challenge
+//
+//  Created by Yosua Hoo on 29/01/19.
+//  Copyright © 2019 Yosua Hoo. All rights reserved.
+//
+
+import UIKit
+
+class FoodMightLikeFOCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var foodImage: UIImageView!
+    
+    @IBOutlet weak var locationDesc: UILabel!
+    @IBOutlet weak var foodName: UILabel!
+}
